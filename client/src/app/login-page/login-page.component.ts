adcb0c256bfa2d26be1d76d6c66a406d005c239e
CampbellKT95 <campbellkt2013@gmail.com>
2022-08-06 12:32:36 -0400

    built simple v1 of login page

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,17 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+export class LoginPageComponent {
+    // moves label when an input is focused, purely cosmetic
+    usernameLabelShift = false;
+    passwordLabelShift = false;
 
-  ngOnInit(): void {
-  }
+    shiftLabel(label: string){
+      if (label === "username") {
+        this.usernameLabelShift = true;
+      } else if (label === "password") {
+        this.passwordLabelShift = true;
+      }
+    }
 
-  test = "test";
+    // login credentials to be passed to auth API
+    loginData = new FormGroup({
+        username : new FormControl(''),
+        password : new FormControl('')
+    })
 
+    // test that this actually works with the correct form data
+    // use https://httpbin.org/post, which supposedly echos POST content
+    authenticateUser() {
+      fetch("https://httpbin.org/post", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.loginData.value)
+      });
+    }
 }
