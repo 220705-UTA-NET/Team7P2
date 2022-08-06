e31ca5f901d88a822497af9b2318b978b8641cce
unknown <dejesusellery@gmail.com>
2022-08-06 11:35:56 -0400

    Added Derick's Models as Project

--- server/Project3.Model/Order.cs
+++ server/Project3.Model/Order.cs
@@ -9,20 +9,28 @@ namespace Project3.Model
     /*
      * Object representation of a order made by a customer.
      * 
-     * @author Ellery R. De Jesus, Derick
+     * @author Derick Xie, Ellery De Jesus
      * @version 8-05-2022
      */
     public class Order
     {
         // The ID of the order
-        public int Order_ID { get; set; }
+        public int id { get; set; }
         // The ID of the customer who made the order
-        public int Customer_ID { get; set; }
+        public int customer_id { get; set; }
         // The date that the order was made
-        public string Order_Date { get; set; }
-        public int Jewelry_ID { get; set; }
-        public int Item_ID { get; set; }
+        public DateTime order_date { get; set; }
+        // The ID of the jewelry that was purchased
+        public int jewelry_id { get; set; }
 
         public Order() { }
+
+        public Order(int id, int customer_id, DateTime order_date, int jewelry_id)
+        {
+            this.id = id;
+            this.customer_id = customer_id;
+            this.order_date = order_date;
+            this.jewelry_id = jewelry_id;
+        }
     }
 }
