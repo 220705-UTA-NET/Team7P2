4cd009019169745519d82b481c6809e1f97b67da
CampbellKT95 <campbellkt2013@gmail.com>
2022-08-07 14:37:41 -0400

    rough initial outline of product page

---
+++ client/src/app/product-page/testJson.ts
@@ -0,0 +1,40 @@
+let testJson = [
+    {
+        "name": "Name",
+        "price": 10,
+        "type": "ring",
+        "material": "gold"
+    },
+    {
+        "name": "Name",
+        "price": 10,
+        "type": "ring",
+        "material": "gold"
+    },
+    {
+        "name": "Name",
+        "price": 10,
+        "type": "ring",
+        "material": "gold"
+    },
+    {
+        "name": "Name",
+        "price": 10,
+        "type": "ring",
+        "material": "gold"
+    },
+    {
+        "name": "Name",
+        "price": 10,
+        "type": "ring",
+        "material": "gold"
+    },
+    {
+        "name": "Name",
+        "price": 10,
+        "type": "ring",
+        "material": "gold"
+    }
+]
+
+export default testJson;
