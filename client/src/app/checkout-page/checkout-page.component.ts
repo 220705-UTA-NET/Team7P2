961d5f05e69581050aed25d54f23ee84c3cd6c90
CampbellKT95 <campbellkt2013@gmail.com>
2022-08-08 12:07:45 -0400

    added checkout & confirmation skeleton

--- client/src/app/checkout-page/checkout-page.component.ts
+++ client/src/app/checkout-page/checkout-page.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+// for testing only
+import {testJson, cart} from "../product-page/testJson";
 
 @Component({
   selector: 'app-checkout-page',
   templateUrl: './checkout-page.component.html',
   styleUrls: ['./checkout-page.component.css']
 })
-export class CheckoutPageComponent implements OnInit {
+export class CheckoutPageComponent {
+  products = testJson;
+  cart = cart;
+  cartPriceTotal = this.cartTotal();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  cartTotal() {
+    let cartPriceTotal = 0;
+    cart.forEach(cartItem => cartPriceTotal += cartItem.price)
+    return cartPriceTotal;
   }
+
+  // send request to Stripe API
+  // set up confirmation modal
+  confirmationModal = false;
+  makePurchase() {
+
+
+    this.confirmationModal = !this.confirmationModal;
+  }
+
+  // re-route user back to product page, but maintain login
+  cancelOrder() {}
+
+  // return user back to product page with an empty cart
+  continueShopping() {}
 
 }
