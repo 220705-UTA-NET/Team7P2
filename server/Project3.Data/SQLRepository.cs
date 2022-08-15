697454697c0f99e7954d87082229599684d0a998
Kadin Campbell <88049534+CampbellKT95@users.noreply.github.com>
2022-08-15 17:06:33 -0400

    Moved sql store parameters to before command fires

--- server/Project3.Data/SQLRepository.cs
+++ server/Project3.Data/SQLRepository.cs
@@ -40,10 +40,10 @@ namespace Project3.Data
 
             SqlCommand cmd = new SqlCommand(cmdText, connection);
 
-            using SqlDataReader reader = await cmd.ExecuteReaderAsync();
-            
             cmd.Parameters.AddWithValue("@startrow", startrow);
             cmd.Parameters.AddWithValue("@endrow", endrow);
+
+            using SqlDataReader reader = await cmd.ExecuteReaderAsync();
 
             Jewelry NewJewelry;
             while (await reader.ReadAsync())
